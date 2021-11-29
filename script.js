a54c7c2b556b553a98e28edc7c673c6988f8c8b6
Mike Mamrila <mmamrila@gmail.com>
2021-11-28 18:16:21 -0700

    transfer html and css from repl.it and finished add new item section

---
+++ script.js
@@ -0,0 +1,9 @@
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+  alert("You must write something!");
+} else {
+  $('#list').append(li);
+}
