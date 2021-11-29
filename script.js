bcc91fa52a4369d603cd4692e166265d662b36fe
Mike Mamrila <mmamrila@gmail.com>
2021-11-28 18:58:29 -0700

    cross out button function

--- script.js
+++ script.js
@@ -16,7 +16,14 @@ function newItem() {
 		li.toggleClass("strike");
 	});
 
+  //Add Cross Out Button
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+      li.addClass('delete')
+  }
+  $('#list').sortable();
 }
